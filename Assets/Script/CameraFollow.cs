80402d58a83a6848c19586351c855d3645f9a1f2
KRTQuant <61773202+KRTQuant@users.noreply.github.com>
2020-04-18 01:20:24 +0700

    update 101
    fix camera stop at end of map

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Vector2 velocity;
-
+    //======================= SmoothDamp =====================================
     public float smoothTimeX;
     public float smoothTimeY;
 
+    //======================= Clamp ========================================
+    public float minX, minY, maxX, maxY;
+
     public GameObject player;
+    public Transform targetForClamp;
+
+    private void Start()
+    {
+        targetForClamp = GameObject.Find("Player").transform;
+    }
 
     private void Update()
+    {
+
+    }
+
+    private void SmoothDamp()
     {
         float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
         float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
         transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+
+    private void Clamp()
+    {
+        transform.position = new Vector3(Mathf.Clamp(targetForClamp.position.x, minX, maxX), Mathf.Clamp(targetForClamp.position.y, minY, maxY), transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        Clamp();
     }
 }
